7834754e29bb57892e71d1993f16b724ed75adab
Abhishek Hazra <abhishek.hazra@awesomesuite.com>
2023-06-12 10:07:42 +0530

    Login Pages Done

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
 import "./Login.css";
 
 const Login = () => {
@@ -6,7 +7,40 @@ const Login = () => {
     email: "",
     password: "",
   });
-  return <div className="mainEntry"></div>;
+  const history = useHistory();
+  const handleSignUp = () => {
+    history.push("/signup");
+  };
+  const handleSignIn = () => {
+    history.push("/home");
+  };
+  return (
+    <div className="mainEntry">
+      <div className="mainEntryContainer">
+        <div className="entryTop">
+          <div className="entryTitle">{"Hello Again"}</div>
+          <div className="entryDescription">{"Welcome back to RentPG !!!"}</div>
+        </div>
+        <div className="entryData">
+          <div className="entryDataUnit">
+            <input type="text" placeholder="Email" />
+          </div>
+          <div className="entryDataUnit">
+            <input type="password" placeholder="Password" />
+          </div>
+          <div className="entryDataButton">
+            <button onClick={handleSignIn}>Sign In</button>
+            <p>
+              Not a Member ?
+              <span onClick={handleSignUp} className="register">
+                Register Now
+              </span>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
---
+++ src/Pages/Login/Signup.js
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import "./Login.css";
+
+const Signup = () => {
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+  const history = useHistory();
+  const handleSignUp = () => {
+    history.push("/signup");
+  };
+  const handleSignIn = () => {
+    history.push("/login");
+  };
+  return (
+    <div className="mainEntry">
+      <div className="mainEntryContainer">
+        <div className="entryTop">
+          <div className="entryTitle">{"Hello Amigo !"}</div>
+          <div className="entryDescription">{"Welcome to RentPG "}</div>
+        </div>
+        <div className="entryData">
+          <div className="entryDataUnit">
+            <input type="text" placeholder="First Name" />
+          </div>
+          <div className="entryDataUnit">
+            <input type="text" placeholder="Last Name" />
+          </div>
+          <div className="entryDataUnit">
+            <input type="text" placeholder="Email" />
+          </div>
+          <div className="entryDataUnit">
+            <input type="password" placeholder="Password" />
+          </div>
+          <div className="entryDataButton">
+            <button onClick={handleSignUp}>Sign Up</button>
+            <p>
+              Already a Member ?
+              <span onClick={handleSignIn} className="register">
+                SignIn Now
+              </span>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Signup;
