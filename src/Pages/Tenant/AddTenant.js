7f826f2589bba93cfc472a4c089fc959b8f940ae
Abhishek Hazra <enigma.abhi.ty@gmail.com>
2023-06-14 14:14:25 +0530

    Tenant Page Added

--- src/Pages/Tenant/AddTenant.js
+++ src/Pages/Tenant/AddTenant.js
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Tenant.css";
-
+import moment from "moment/moment";
 import Header from "../../Components/Header/Header";
 import Footer from "../../Components/Footer/Footer";
 
 const AddTenant = () => {
+  const [currentDate, setCurrentDate] = useState(moment().format("YYYY-MM-DD"));
+  const onChangeDate = (e) => {
+    const d = new Date(e.target.value);
+    console.log(d.getMonth() + 1);
+    console.log(d.getDate());
+    const newDate = moment(new Date(e.target.value)).format("YYYY-MM-DD");
+    setCurrentDate(newDate);
+    console.log(newDate); //value picked from date picker
+  };
   return (
     <div className="tenantMain">
       <Header />
@@ -23,7 +32,7 @@ const AddTenant = () => {
         </div>
         <div className="tenantAddSection">
           <p>Date of Joining</p>
-          <input type="date" />
+          <input type="date" value={currentDate} onChange={onChangeDate} />
         </div>
         <div className="tenantAddHalf">
           <div className="tenantAddLeftSection">
@@ -33,6 +42,41 @@ const AddTenant = () => {
           <div className="tenantAddRightSection">
             <p>Security Deposit</p>
             <input type="number" />
+          </div>
+        </div>
+        <div className="tenantBalanceHeader">
+          <p>Opening Balance of Tenant</p>
+        </div>
+        <div className="tenantBalanceSection">
+          <div className="section">
+            <div className="sectionUnitHeader">Dues Type</div>
+            <div className="sectionUnitHeader">Due</div>
+            <div className="sectionUnitHeader">Collected</div>
+          </div>
+          <div className="section">
+            <div className="sectionUnit unitMain">Rent</div>
+            <div className="sectionUnit">
+              <p className="rate">Rs 400</p>
+              <p className="range">14 June to 30 June</p>
+            </div>
+            <div className="sectionUnit collected">
+              <img src="Assets/Tenant/edit.png" />
+              <p>0</p>
+            </div>
+          </div>
+          <div className="section">
+            <div className="sectionUnit unitMain">Security Deposit</div>
+            <div className="sectionUnit">
+              <p className="rate">Rs 400</p>
+              <p className="range">14 June to 30 June</p>
+            </div>
+            <div className="sectionUnit collected">
+              <img src="Assets/Tenant/edit.png" />
+              <p>0</p>
+            </div>
+          </div>
+          <div className="tenantButton">
+            <button>Add Tenant</button>
           </div>
         </div>
       </div>
