258bdbb2c921f92966ead3977eb8aadfbdb7bc9f
Abhishek Hazra <enigma.abhi.ty@gmail.com>
2023-07-12 21:54:40 +0530

    Food Section

--- src/Pages/Food/FoodListTest.js
+++ src/Pages/Food/FoodListTest.js
@@ -1,32 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Carousel from "../../Components/Carousel/Carousel";
 const FoodListTest = () => {
-  const settings = {
-    dots: false,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-  };
+  const daysArray = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+  const [days, setDays] = useState(0);
   return (
     <div className="foodListContainer">
       <div className="listDays">
-        <div className="listUnits listUnitActive">{"Mon"}</div>
-        <div className="listUnits">{"Tue"}</div>
-        <div className="listUnits">{"Wed"}</div>
-        <div className="listUnits">{"Thu"}</div>
-        <div className="listUnits">{"Fri"}</div>
-        <div className="listUnits">{"Sat"}</div>
-        <div className="listUnits">{"Sun"}</div>
+        <div
+          className={`listUnits ${
+            daysArray[days] == "Mon" ? "listUnitActive" : ""
+          }`}
+        >
+          {"Mon"}
+        </div>
+        <div
+          className={`listUnits ${
+            daysArray[days] == "Tue" ? "listUnitActive" : ""
+          }`}
+        >
+          {"Tue"}
+        </div>
+        <div
+          className={`listUnits ${
+            daysArray[days] == "Wed" ? "listUnitActive" : ""
+          }`}
+        >
+          {"Wed"}
+        </div>
+        <div
+          className={`listUnits ${
+            daysArray[days] == "Thu" ? "listUnitActive" : ""
+          }`}
+        >
+          {"Thu"}
+        </div>
+        <div
+          className={`listUnits ${
+            daysArray[days] == "Fri" ? "listUnitActive" : ""
+          }`}
+        >
+          {"Fri"}
+        </div>
+        <div
+          className={`listUnits ${
+            daysArray[days] == "Sat" ? "listUnitActive" : ""
+          }`}
+        >
+          {"Sat"}
+        </div>
+        <div
+          className={`listUnits ${
+            daysArray[days] == "Sun" ? "listUnitActive" : ""
+          }`}
+        >
+          {"Sun"}
+        </div>
       </div>
-      <Carousel>
+      <Carousel setData={setDays}>
         <FoodListPage day={"Monday"} />
         <FoodListPage day={"Tuesday"} />
         <FoodListPage day={"Wednesday"} />
         <FoodListPage day={"Thursday"} />
+        <FoodListPage day={"Friday"} />
+        <FoodListPage day={"Saturday"} />
+        <FoodListPage day={"Sunday"} />
       </Carousel>
     </div>
   );
@@ -38,14 +78,14 @@ const FoodListPage = ({ day }) => {
       <div className="foodListTitle">
         <p>{day}</p>
       </div>
-      <FoodListUnit />
-      <FoodListUnit />
-      <FoodListUnit />
-      <FoodListUnit />
+      <FoodListUnit meal={"BreakFast"} />
+      <FoodListUnit meal={"Lunch"} />
+      <FoodListUnit meal={"Snacks"} />
+      <FoodListUnit meal={"Dinner"} />
     </div>
   );
 };
-const FoodListUnit = () => {
+const FoodListUnit = ({ meal }) => {
   return (
     <div className="foodListUnit">
       <div className="listImg">
@@ -53,7 +93,7 @@ const FoodListUnit = () => {
       </div>
       <div className="listDetails">
         <div className="listDetailsTop">
-          <div className="mealType">{"Breakfast"}</div>
+          <div className="mealType">{meal}</div>
           <div className="mealTime">{"8:00 AM to 10:00 AM"}</div>
         </div>
         <div className="listDetailsBottom">
