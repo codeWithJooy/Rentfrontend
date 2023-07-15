d8ebcd9249ab14ab61a953837d672bc430167f36
Abhishek Hazra <enigma.abhi.ty@gmail.com>
2023-07-15 21:38:35 +0530

    Food

---
+++ src/data/announcementData.js
@@ -0,0 +1,17 @@
+export const announcementData = [
+  {
+    title: "Happy Diwali",
+    messgae: "Happy Diwali to All of You",
+    tag: "Festival",
+  },
+  {
+    title: "Happy New Year",
+    messgae: "Happy New Year to All of You",
+    tag: "Festival",
+  },
+  {
+    title: "Lunch Is ready",
+    messgae: "Dear Tenat, Today's lunch is hot and ready for you",
+    tag: "Festival",
+  },
+];
--- src/reducers/foodReducer.js
+++ src/reducers/foodReducer.js
@@ -1,4 +1,4 @@
-import { SET_FOOD_DATA } from "../actionTypes/foodActionType";
+import { SET_FOOD_DATA, SET_FOOD_TIME } from "../actionTypes/foodActionType";
 
 const initial = {
   activated: false,
@@ -8,6 +8,11 @@ const foodReducer = (state = initial, action) => {
   switch (action.type) {
     case SET_FOOD_DATA:
       return action.payload;
+    case SET_FOOD_TIME:
+      return {
+        ...state,
+        timeActivated: true,
+      };
     default:
       return state;
   }
