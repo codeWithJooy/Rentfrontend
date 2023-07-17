333098f90db314602a0bbd1451b32c7f7047f032
Abhishek Hazra <enigma.abhi.ty@gmail.com>
2023-07-17 20:10:51 +0530

    Dues Changes

--- src/actions/collectionAction.js
+++ src/actions/collectionAction.js
@@ -7,6 +7,47 @@ import { CodeAnalogy } from "../Components/Toasty/Toasty";
 import { tenantApi, collectionApi } from "../apis/apis";
 import { getHeaders, dispatchAction } from "./actionHelper";
 
+export const addCollection = async (
+  userId,
+  propertyId,
+  tenantId,
+  type,
+  amount,
+  date,
+  mode
+) => {
+  try {
+    console.log("Type is ", type);
+    const headers = getHeaders({
+      userId,
+      propertyId,
+      tenantId,
+    });
+    const data = {
+      type,
+      amount,
+      date,
+      mode,
+    };
+    const res = await collectionApi.post("/addCollection", data, headers);
+    if (res.data.code == 200) {
+      updateToast({
+        code: CodeAnalogy.SUCCESS,
+        title: "Payment Added",
+        message: "Payment Done Successfully",
+      });
+      return true;
+    } else {
+      updateToast({
+        code: CodeAnalogy.ERROR,
+        title: "Something Went Wrong",
+        message: "Cannot Process payment",
+      });
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
 export const getTenantCollection = async (userId, propertyId, tenantId) => {
   try {
     const headers = getHeaders({
@@ -36,7 +77,8 @@ export const getAllCollections = async (userId, propertyId) => {
     });
     const res = await collectionApi.get("/getAllCollection", headers);
     if (res.data.code == 200) {
-      dispatchAction(SET_ALL_COLLECTION, res.data.code);
+      dispatchAction(SET_ALL_COLLECTION, res.data.model);
+      return res.data.model;
     } else {
       updateToast({
         code: CodeAnalogy.ERROR,
