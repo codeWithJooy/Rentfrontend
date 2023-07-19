7a9bb754dc9b0d787cb9a42c7217d5f116679b62
Abhishek Hazra <enigma.abhi.ty@gmail.com>
2023-07-19 12:55:44 +0530

    Property Add Flickering Issue

--- src/Pages/Property/Property.js
+++ src/Pages/Property/Property.js
@@ -1,17 +1,31 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./Property.css";
+import { useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
 import Header from "../../Components/Header/Header";
 import Footer from "../../Components/Footer/Footer";
 import PropertyInit from "./PropertyInit";
-
+import FloorSkeleton from "../../Components/Skeletons/FloorSkeleton";
 const Property = () => {
-  return (
-    <div className="propertyHome">
-      <Header />
-      <PropertyInit />
-      <Footer page={"Property"} />
-    </div>
-  );
+  const history = useHistory();
+  const floorPresent = useSelector((state) => state.floor.floorPresent);
+  useEffect(() => {
+    if (floorPresent) {
+      history.push("/floor");
+    }
+  }, []);
+  if (!floorPresent) {
+    return (
+      <div className="propertyHome">
+        <Header />
+        {!floorPresent && <PropertyInit />}
+
+        <Footer page={"Property"} />
+      </div>
+    );
+  } else {
+    return <FloorSkeleton />;
+  }
 };
 
 export default Property;
