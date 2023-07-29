46fd12d15f0a0fcfa75679aa519e17a89999173e
Abhishek Hazra <enigma.abhi.ty@gmail.com>
2023-07-29 12:20:12 +0530

    Studen Action Created

---
+++ src/actions/Student/studentAction.js
@@ -0,0 +1,64 @@
+import {
+  COMPLAINT_TYPE,
+  CODE_NUMBER_CHECK,
+} from "../../actionTypes/studentActionType";
+import { dispatchAction, getHeaders } from "../actionHelper";
+import { studentApi } from "../../apis/apis";
+import { updateToast } from "../toastActions";
+import { CodeAnalogy } from "../../Components/Toasty/Toasty";
+export const setComplaintType = (data) => {
+  return {
+    type: COMPLAINT_TYPE,
+    payload: data,
+  };
+};
+
+export const checkCodeNumber = async (number, code) => {
+  try {
+    const headers = getHeaders({
+      code,
+      number,
+    });
+    const res = await studentApi.get("/checkCodeNumber", headers);
+    if (res.data.code == 200) {
+      dispatchAction(CODE_NUMBER_CHECK, res.data.model);
+      updateToast({
+        code: CodeAnalogy.SUCCESS,
+        title: `Welcome to ${res.data.model.propertyName}`,
+        message: "Please make Sure code is correct.",
+      });
+      return true;
+    } else {
+      updateToast({
+        code: CodeAnalogy.ERROR,
+        title: "Something Went Wrong",
+        message: "Please make Sure code is correct.",
+      });
+      return false;
+    }
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+export const addStudent = async (data) => {
+  try {
+    const res = await studentApi.post("/addStudent", data);
+    if (res.data.code == 200) {
+      dispatchAction(CODE_NUMBER_CHECK, res.data.model);
+      updateToast({
+        code: CodeAnalogy.SUCCESS,
+        title: `Successfully Signed Up`,
+      });
+      return true;
+    } else {
+      updateToast({
+        code: CodeAnalogy.ERROR,
+        title: "Something Went Wrong",
+        message: "Please try again later",
+      });
+      return false;
+    }
+  } catch (error) {
+    console.log(error.message);
+  }
+};
