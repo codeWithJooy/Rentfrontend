7a60ab78835074f2dfaa6eb3d9944ec31c633fda
Abhishek Hazra <enigma.abhi.ty@gmail.com>
2023-07-29 12:20:49 +0530

    Student Reducer Updated

--- src/reducers/studentReducer.js
+++ src/reducers/studentReducer.js
@@ -1,7 +1,11 @@
-import { COMPLAINT_TYPE } from "../actionTypes/studentActionType";
+import {
+  COMPLAINT_TYPE,
+  CODE_NUMBER_CHECK,
+} from "../actionTypes/studentActionType";
 
 const initial = {
   complaintType: "",
+  studentData: {},
 };
 
 const studentReducer = (state = initial, action) => {
@@ -10,6 +14,11 @@ const studentReducer = (state = initial, action) => {
       return {
         ...state,
         complaintType: action.payload,
+      };
+    case CODE_NUMBER_CHECK:
+      return {
+        ...state,
+        studentData: action.payload,
       };
     default:
       return state;
