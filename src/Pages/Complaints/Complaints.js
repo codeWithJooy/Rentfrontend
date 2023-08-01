283577d69890618ecbd961e3775e0de5564baa89
Abhishek Hazra <enigma.abhi.ty@gmail.com>
2023-08-01 23:36:37 +0530

    Complaint Page

---
+++ src/Pages/Complaints/Complaints.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import "./Complaints.css"
+import Header from '../../Components/Header/Header'
+import Footer from '../../Components/Footer/Footer'
+
+const Complaints = () => {
+    return (
+        <div className='comMain'>
+            <Header type="back" name="Complaints" link="/home"/>
+            <div className='comContainer'>
+
+            </div>
+            <Footer />
+        </div>
+    )
+}
+
+export default Complaints
+
