eaaee0bab414e702e5d6a3fec53ce099b2ee1c26
Abhishek Hazra <enigma.abhi.ty@gmail.com>
2023-08-02 00:33:47 +0530

    Complaint Page

--- src/Pages/Complaints/Complaints.js
+++ src/Pages/Complaints/Complaints.js
@@ -8,7 +8,7 @@ const Complaints = () => {
         <div className='comMain'>
             <Header type="back" name="Complaints" link="/home"/>
             <div className='comContainer'>
-
+                <ComplaintCard />
             </div>
             <Footer />
         </div>
@@ -17,3 +17,38 @@ const Complaints = () => {
 
 export default Complaints
 
+const ComplaintCard = () => {
+    return (
+        <div className='comCard'>
+            <div className='comCardTop'>
+                <div className='comCardImg'>
+                  <img src="Assets/components/picture.png" />
+                </div>
+                <div className='comCardText'>
+                    <div className='comCardType'>
+                       <span>Furniture</span> <span>Table</span>
+                    </div>
+                    <div className='comCardDescription'>
+                      <p>Very Dirty FUrniture full of bugs.</p>
+                    </div>
+                </div>
+            </div>
+            <div className='comCardBottom'>
+                <div className='comCardName'>
+                    <p>Abhishek Hazra</p>
+                </div>
+                <div className='comCardRoom'>
+                    <p>Base:102</p>
+                </div>
+            </div>
+            <div className='comCardStatus'>
+                <div className='comStatusTitle'>
+                    <p>Status</p>
+                </div>
+                <div className='comStatusSelect'>
+                    <select></select>
+                </div>
+            </div>
+        </div>
+    )
+}
