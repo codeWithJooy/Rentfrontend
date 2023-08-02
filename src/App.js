79d74cf57e2516108f4adec0bfb9709d086c22cd
Abhishek Hazra <enigma.abhi.ty@gmail.com>
2023-08-02 08:59:55 +0530

    Profile Page Path added in App.js

--- src/App.js
+++ src/App.js
@@ -44,6 +44,7 @@ import StudentSignUp from "./Pages/Student/Signup/StudentSignUp";
 import StudentLogin from "./Pages/Student/Signup/StudentLogin";
 import StudentNumberLogin from "./Pages/Student/Signup/StudentNumberLogin";
 import StudentAccount from "./Pages/Student/Account/StudentAccount";
+import Profile from "./Pages/Profile/Profile";
 const App = () => {
   return (
     <BrowserRouter>
@@ -78,7 +79,7 @@ const App = () => {
         <Route path="/addann" component={AddAnnouncement} />
         
         <Route path="/complaintSection" component={Complaints} />
-        
+        <Route path="/profile" component={Profile} />
         <Route path="/receipt" component={Receipt} />
 
         <Route path="/studentSignUp" component={StudentSignUp} />
