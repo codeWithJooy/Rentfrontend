25ea1ab96aa292cdca668ab0cf38591e0a7c57d6
Abhishek Hazra <enigma.abhi.ty@gmail.com>
2023-08-03 15:17:55 +0530

    Complaint Notification Done

--- src/Pages/Notifications/ComplaintNotiSection.js
+++ src/Pages/Notifications/ComplaintNotiSection.js
@@ -1,40 +1,60 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
+import { useSelector } from 'react-redux';
+import { getComplaints } from '../../actions/Student/studentAction';
+import { useHistory } from "react-router-dom"
 
 const ComplaintNotiSection = () => {
+    const { userId, propertyId } = useSelector(state => state.user)
+    const [tempData, setTempData] = useState([])
+    useEffect(() => {
+        (async () => {
+            let data = await getComplaints(userId, propertyId)
+            setTempData(data)
+        })()
+    },[])
     return (
         <div className='paymentNotiSection'>
-          <ComplaintNotiCard />
+             {
+                tempData.length > 0 &&
+                tempData.reverse().map((data, key) => (
+                    <ComplaintNotiCard type={data.type} subType={data.subType} name={data.name} room={data.room} date={data.date} status={data.status} key={key} />
+                    ))
+                }
         </div>
     )
 }
 
 export default ComplaintNotiSection;
 
-const ComplaintNotiCard = () => {
+const ComplaintNotiCard = ({ type, subType, name, room, date, status }) => {
+    const history = useHistory()
+    const handleClick = () => {
+        history.push("/complaintSection")
+    }
     return (
-        <div className='paymentNotiCard'>
+        <div className='paymentNotiCard' onClick={handleClick}>
             <div className='paymentDetails'>
                 <div className='payType'>
-                   <p>Bedroom</p>
+                   <p>{type}</p>
                 </div>
                 <div className='payAmount'>
-                   <p>Cleanliness</p>
+                    <p>{subType}</p>
                 </div>
             </div>
             <div className='payeeDetails'>
                 <div className='payeeName'>
-                   <p>Abhishek Hazra</p>
+                    <p>{name}</p>
                 </div>
                 <div className='payeeRoom'>
-                   <p>G-101</p>
+                    <p>{room}</p>
                 </div>
             </div>
             <div className='paymentMeta'>
                 <div className='paymentMetaDate'>
-                   <p>Raise On:1/08/2023</p>
+                    <p>Raise On:{date}</p>
                 </div>
                 <div className='paymentMetaMode'>
-                   <p>Status</p>
+                    <p>{status}</p>
                 </div>
             </div>
         </div>
