70fd4be554f1942ab4150c908a00b9126c2b7d01
Abhishek Hazra <abhishek.hazra@awesomesuite.com>
2023-08-22 14:00:39 +0530

    DuesUnitTenant Change

--- src/Components/Dues/DuesUnitTenanat.js
+++ src/Components/Dues/DuesUnitTenanat.js
@@ -4,6 +4,7 @@ import "./DuesRoom.css";
 import { getAllRooms, getRoomName } from "../../actions/roomActions";
 import { getTenants } from "../../actions/tenantAction";
 import { calculateTotalDues } from "../../helper";
+import { getDuesTenant } from "../../actions/duesAction";
 const DuesUnitTenant = ({ setOpen, setDueSetData }) => {
   const user = useSelector((state) => state.user);
   const [tenants, setTenants] = useState([]);
@@ -34,6 +35,8 @@ export default DuesUnitTenant;
 
 const TenantDuesCard = ({ data, setOpen, setDueSetData }) => {
   const [roomName, setRoomName] = useState("");
+  const [dues, setDues] = useState([])
+  const [forceUpdate, setForceUpdate] = useState(true)
   const { userId, propertyId } = useSelector((state) => state.user);
   const obj = {
     title: `Tenant : ${data.name}`,
@@ -49,29 +52,39 @@ const TenantDuesCard = ({ data, setOpen, setDueSetData }) => {
   };
   useEffect(() => {
     (async () => {
-      setRoomName(await getRoomName(userId, propertyId, data.roomId));
+      if (forceUpdate) {
+        let dueData = await getDuesTenant(userId, propertyId, data._id)
+        setDues(dueData)
+        setRoomName(await getRoomName(userId, propertyId, data.roomId));
+        dueData && setForceUpdate(false)
+      }
     })();
-  }, []);
-  return (
-    <div className="roomDuesCard">
-      <div className="roomDuesTop">
-        <div className="roomDuesroom">
-          <p>{data.name}</p>
+  }, [dues]);
+  if (!forceUpdate) {
+    return (
+      <div className="roomDuesCard">
+        <div className="roomDuesTop">
+          <div className="roomDuesroom">
+            <p>{data.name}</p>
+          </div>
+          <div className="roomDuesButton">
+            <button className="buttonPresent" onClick={handleDueTenant}>
+              Add Dues
+            </button>
+          </div>
         </div>
-        <div className="roomDuesButton">
-          <button className="buttonPresent" onClick={handleDueTenant}>
-            Add Dues
-          </button>
+        <div className="roomDuesBottom">
+          <div className="roomDuesExtra">
+            <p>Dues : Rs {dues ? calculateTotalDues(dues) : 0}</p>
+          </div>
+          <div className="roomDuesRoom">
+            <p>Room: {roomName}</p>
+          </div>
         </div>
       </div>
-      <div className="roomDuesBottom">
-        <div className="roomDuesExtra">
-          <p>Dues : Rs {calculateTotalDues(data.dues)}</p>
-        </div>
-        <div className="roomDuesRoom">
-          <p>Room: {roomName}</p>
-        </div>
-      </div>
-    </div>
-  );
+    );
+  }
+  else {
+    return <></>
+  }
 };
